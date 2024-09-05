a88b3c2ea6e9e0ac1481aeece4c338fc8fc5b888
JerinJoe <jerinjoesv@gmail.com>
2024-09-05 12:35:42 +0530

    complete example to extract data from own website

--- Pyscrape/scrape.py
+++ Pyscrape/scrape.py
@@ -11,6 +11,10 @@ with open('index.html', 'r') as f:
     # print(soup.prettify())
 
     # tags = soup.find('h5')
-    courses_html_tags = soup.find_all('h5')
-    for course in courses_html_tags:
-        print(course.text)
+    course_cards = soup.find_all('div', class_='card')
+
+    for course in course_cards:
+        course_title = course.h5.text
+        course_price = course.a.text.split()[-1]
+
+        print(f'{course_title} + costs + {course_price}')
